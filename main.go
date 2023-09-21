ac7a208a869ab83443d98d341affeb2b00aed9d6
Aaron Lin <aaronlin256@gmail.com>
2023-09-21 15:21:30 -0400

    Add comments, use filepath join

--- main.go
+++ main.go
@@ -3,49 +3,58 @@ package main
 import (
     "fmt"
     "os"
+    "path/filepath"
 )
 
 func main() {
+    // Check for the command-line argument
     if len(os.Args) != 2 {
         fmt.Println("Usage: extract_pngs <executable_path>")
         return
     }
 
+    // Get the executable path from the command-line argument
     exePath := os.Args[1]
 
+    // Read the entire executable file
     exeData, err := os.ReadFile(exePath)
     if err != nil {
         fmt.Printf("Error reading executable: %v\n", err)
         return
     }
 
+    // Define PNG signature bytes
     pngStartSig := []byte{0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A}
     pngEndSig := []byte{0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82}
 
+    // Initialize a counter for extracted PNGs
     var pngCount int
 
-    // Create the "resources" folder if it doesn't exist
+    // Create a "resources" folder if it doesn't exist
     resourcesFolder := "resources"
     if _, err := os.Stat(resourcesFolder); os.IsNotExist(err) {
         os.Mkdir(resourcesFolder, os.ModePerm)
     }
 
+    // Search for PNG signatures in the executable
     for i := 0; i < len(exeData)-len(pngStartSig); i++ {
         if bytesEqual(exeData[i:i+len(pngStartSig)], pngStartSig) {
-            // Found a PNG start signature, look for the end signature
+            // Found the start of a PNG
             for j := i + len(pngStartSig); j < len(exeData)-len(pngEndSig); j++ {
                 if bytesEqual(exeData[j:j+len(pngEndSig)], pngEndSig) {
-                    // Found a PNG image
+                    // Found the end of the PNG
                     pngCount++
                     pngData := exeData[i : j+len(pngEndSig)]
 
-                    // Save the PNG image in the "resources" folder
-                    fileName := fmt.Sprintf("resources/extracted_png_%d.png", pngCount)
-                    err := os.WriteFile(fileName, pngData, 0644)
+                    // Define the path to save the PNG image
+                    pngFileName := filepath.Join(resourcesFolder, fmt.Sprintf("extracted_png_%d.png", pngCount))
+
+                    // Save the PNG image
+                    err := os.WriteFile(pngFileName, pngData, 0644)
                     if err != nil {
-                        fmt.Printf("Error saving PNG file %s: %v\n", fileName, err)
+                        fmt.Printf("Error saving PNG file %s: %v\n", pngFileName, err)
                     } else {
-                        fmt.Printf("Saved %s\n", fileName)
+                        fmt.Printf("Saved %s\n", pngFileName)
                     }
 
                     // Move to the next position after the end signature
